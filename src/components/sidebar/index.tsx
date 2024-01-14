19a10cb3194d35bd2d87a44393a01f8bd72c052b
Saffet01 <saffetr.kaban@gmail.com>
2024-01-15 00:05:12 +0300

    Home Component input

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,93 +1,116 @@
 import React from 'react'
-import HomeIcon from '../icons/home-icon';
-import MovieIcon from '../icons/movie-icon';
-import TvSeriesIcon from '../icons/series-icon';
-import BookmarkIcon from '../icons/bookmark-icon';
+import homeIcon from '../../assets/icons/icon-nav-home.svg';
+import movieIcon from '../../assets/icons/icon-nav-movies.svg';
+import tvSeriesIcon from '../../assets/icons/icon-nav-tv-series.svg';
+import bookmarkIcon from '../../assets/icons/icon-nav-bookmark.svg';
 import { Link, useLocation } from 'react-router-dom';
 import { Box, Typography } from '@mui/material';
 
 const navLinks = [
     {
         name: "Home",
-        icon: HomeIcon,
+        icon: homeIcon,
         link: "/",
     },
     {
         name: "Movies",
-        icon: MovieIcon,
+        icon: movieIcon,
         link: "/movies",
     },
     {
         name: "Tv Series",
-        icon: TvSeriesIcon,
+        icon: tvSeriesIcon,
         link: "/tv-series",
     },
     {
         name: "Bookmark",
-        icon: BookmarkIcon,
+        icon: bookmarkIcon,
         link: "/bookmark",
     }
 ];
 
 export const Sidebar = () => {
 
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
 
-  return (
-    <Box sx={{
-        backgroundColor: "161d1f",
-        padding: 2,
-        borderRadius: 2,
-        display: "flex",
-        flexDirection: {
-            xs: "row",
-            lg: "column",
-        },
-        alignItems: "center",
-        justifyContent: "space-between",
-        width: {
-            sm: "100%",
-            lg: 200,
-        },
-    }}>
+    return (
         <Box sx={{
+            backgroundColor: "#161d2f",
+            padding: 3,
+            borderRadius: 2,
             display: "flex",
             flexDirection: {
                 xs: "row",
                 lg: "column",
             },
-            gap: 5,
-            alignItems: {
-                xs: "center",
-                lg: "start",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: {
+                sm: "100%",
+                lg: 200,
             },
-            width: "100%",
         }}>
-            <Typography variant='h5' component="h1" my={2} fontWeight={400} fontSize={18}>
-                <Box sx={{
-                    py: {
-                        xs: "0px",
-                        lg: "16px",
-                    },
-                    display: "flex",
-                    flexDirection: {
-                        xs: "row",
-                        lg: "column",
-                    },
-                    gap: 5,
-                }}>
-                    {navLinks.map( item => (
-                        <Link key={item.name} to={item.link} style={{textDecoration: "none"}}>
-                            <img src={item.icon} alt={item.name} style={{
-                                width: "18px",
-                                filter: `${pathname === item.name ? "invert(58%) sepia(14%) saturate(3166%) hue-rotate(215deg) brightness(91%) contrast(87%)": "invert(84%)"}`,
-                            }} ></img>
-                            <Typography>{item.name}</Typography>
-                        </Link>
-                    ))}
-                </Box>
-            </Typography>
+            <Box sx={{
+                display: "flex",
+                flexDirection: {
+                    xs: "row",
+                    lg: "column",
+                },
+                gap: 5,
+                alignItems: {
+                    xs: "center",
+                    lg: "start",
+                },
+                width: "100%",
+            }}>
+                <Typography variant='h5' component="h1" my={2} fontWeight={400} fontSize={18}>
+                    <Box
+                        sx={{
+                            py: {
+                                xs: "0px",
+                                lg: "16px",
+                            },
+                            display: "flex",
+                            flexDirection: {
+                                xs: "row",
+                                lg: "column",
+                            },
+                            gap: 4,
+                        }}
+                    >
+                        {navLinks.map((item) => (
+                            <Link
+                                key={item.name}
+                                to={item.link}
+                                style={{ textDecoration: "none" }}
+                            >
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        alignItems: "center",
+                                        gap: 2,
+                                        color: "white",
+                                        textDecoration: "none",
+                                    }}
+                                >
+                                    <img
+                                        src={item.icon}
+                                        alt={item.name}
+                                        style={{
+                                            width: "18px",
+                                            filter: `${pathname === item.link
+                                                ? "invert(58%) sepia(14%) saturate(3166%) hue-rotate(215deg) brightness(91%) contrast(87%)"
+                                                : "invert(84%)"
+                                                }`,
+                                        }}
+                                    />
+                                    <Typography>{item.name}</Typography>
+                                </Box>
+                            </Link>
+                        ))}
+                    </Box>
+                </Typography>
+            </Box>
         </Box>
-    </Box>
-  )
+    )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import { Layout } from '../../layout'
+import { InputBase, Paper } from '@mui/material'
+
 
 export const Home = () => {
   return (
-    <Layout>Home</Layout>
+    <Layout>
+      <Paper component="form" sx={{
+        display: "flex",
+        alignItems: "center",
+        borderRadius: "default",
+        padding: 1,
+        backgroundColor: "#10141f",
+        border: "none"
+      }}>
+        
+        <InputBase placeholder='Search for movies or Tv series.' sx={{
+          ml: 1,
+          flex: 1,
+          color: "white",
+          borderBottom: "1px solid white",
+        }}
+        value={search} />
+
+      </Paper>
+    </Layout>
   )
 }
