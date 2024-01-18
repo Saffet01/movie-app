cf16f64897ec8f3d8ac338dc9c8598b3e303e2b2
Saffet01 <saffetr.kaban@gmail.com>
2024-01-19 00:38:16 +0300

    movie context

---
+++ src/context/movie-context.tsx
@@ -0,0 +1,54 @@
+import { ReactNode, createContext, useReducer } from "react";
+import { MovieDataType, moviesData } from "../assets/data";
+import { MovieList } from "../components/movie-list";
+
+interface MovieContextProps {
+    children: ReactNode
+}
+
+interface MovieAction {
+    type: string,
+    id: string
+}
+
+const MovieList: MovieDataType[] = moviesData;
+
+interface MovieState {
+    movies: MovieDataType[];
+}
+
+const initialMovieState: MovieState = {
+    movies: MovieList,
+}
+
+const MovieReducer = (state: MovieState, action: MovieAction): MovieState => {
+    switch (action.type) {
+        case "TOGGLE BOOKMARK":
+            return {
+                ...state,
+                movies: state.movies.map(movie => {
+                    if (movie.id === action.id) {
+                        return { ...movie, isBookmarked: !movie.isBookmarked };
+                    }
+                    return movie;
+                })
+            };
+            default:
+                return state;
+    }
+}
+
+export const MovieContext = createContext<{
+    state: MovieState,
+    dispatch: React.Dispatch<MovieAction>
+}>(
+    {
+        state: initialMovieState,
+        dispatch: () => { }
+    }
+)
+
+export const MovieProvider = ({ children }: MovieContextProps) => {
+    const [state, dispatch] = useReducer(MovieReducer, initialMovieState);
+    return <MovieContext.Provider value={state, dispatch} >{children}</MovieContext.Provider>
+}
